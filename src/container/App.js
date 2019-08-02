f2a624a80d46dd8da598e4409fb8455f5a408c65
Semilore Idowu <semijacks27@gmail.com>
2019-08-02 16:01:38 +0100

    reorganized files

--- src/container/App.js
+++ src/container/App.js
@@ -24,16 +24,16 @@ class App extends Component {
 	};
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robots => {
-			return robots.name
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name
 				.toLowerCase()
-				.includes(this.state.searchfield.toLowerCase());
+				.includes(searchfield.toLowerCase());
 		});
 
-		if (this.state.robots.length === 0) {
-			return <h1>Loading...</h1>;
-		} else {
-			return (
+		return !robots.length ?
+			<h1>Loading...</h1>:
+		(
 				<div className="tc">
 					<h1 className="f1">RoboFriends</h1>
 					<SearchBox searchChange={this.onSearchChange} />
@@ -41,8 +41,7 @@ class App extends Component {
 						<CardList robots={filteredRobots} />
 					</Scroll>
 				</div>
-			);
-		}
+			)
 	}
 }
 
