4cb70bc79bbe6122fba379e689850e9f42cbe5f6
Semilore Idowu <semijacks27@gmail.com>
2019-08-05 11:20:15 +0100

    Updates

---
+++ precache-manifest.8c0b030bfd5486824c3f6ca5969287be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3964142be2b36aa9037a1534e744c498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4885a1782589c28d0d04",
+    "url": "/static/css/main.32837f66.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "4885a1782589c28d0d04",
+    "url": "/static/js/main.69b74a4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
